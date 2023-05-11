64518c4d22ebe376622e1bb89abc5c63963c0e3f
Zara Martín <zmartand@myuax.es>
2023-05-11 19:23:40 +0200

    clase abstracta Elemento

---
+++ src/main/java/org/example/Elemento.java
@@ -0,0 +1,27 @@
+package org.example;
+public abstract class Elemento {
+    protected int codigo;
+    protected double precio;
+
+    public Elemento(int codigo, double precio) {
+        this.codigo = codigo;
+        this.precio = precio;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public abstract double getPrecio();
+
+    public void agregarElemento(Elemento elemento) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+
+    public void eliminarElemento(Elemento elemento) {
+        throw new UnsupportedOperationException("Operación no soportada");
+    }
+    public abstract void mostrar();
+}
+
+
